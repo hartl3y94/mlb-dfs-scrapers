1978c5746eda3a2791fd23b278f6cc7d64fe54fb
Kyle Kosic <kylekosic@gmail.com>
2018-04-09 07:34:32 -0400

    reorganize

---
+++ README.md
@@ -0,0 +1,3 @@
+# MLB Daily Fantasy Stat Scrapers
+
+This repository contains code to fetch MLB stats from a variety of websites and dump them into an S3 bucket. Config files need to be setup in ./config and you need an `accounts.yml` for some websites' login information.
--- util/config.py
+++ util/config.py
@@ -2,6 +2,10 @@ import os
 import yaml
 
 def get_config(path='config.yml'):
+	""" Load a yaml config file with name path
+		from the configs directory
+	"""
+	path = os.path.join('configs', path)
     
     if not os.path.exists(path):
         raise Exception("Missing config file %s" % path)
